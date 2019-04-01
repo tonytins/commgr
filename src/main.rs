03ca3c51565846219ea4de057a0c71810b341720
Anthony Wilcox <35226681+antonwilc0x@users.noreply.github.com>
2019-04-01 09:20:14 -0400

    Created debug flag
    - The debug flag, when combined with YCH, Commission and soon Request flags, will print outputs to the screen. This is to save on the number of writes made to the disk but may be turned into a feature when I switch to a proper database.

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,18 @@ use chrono::prelude::*;
 
 // Argument names
 // =======================
-const COMM_ARG: &str = "commission";
-const REQ_ARG: &str = "request";
-const YCH_ARG: &str = "ych";
+const COMM_FLAG: &str = "commission";
+const REQ_FLAG: &str = "request";
+const YCH_FLAG: &str = "ych";
+const DEBUG_FLAG: &str = "debug";
 
-const BUYER_ARG: &str = "buyer";
-const UNAME_ARG: &str = "username";
-const ORDER_ARG: &str = "order";
-const PAYMENT_ARG: &str = "payment";
-const COST_ARG: &str = "cost";
-const SLOT_ARG: &str = "slot";
-// const RAFFLE_ARG: &str = "raffle";
+const BUYER_OPT: &str = "buyer";
+const UNAME_OPT: &str = "username";
+const ORDER_OPT: &str = "order";
+const PAYMENT_OPT: &str = "payment";
+const COST_OPT: &str = "cost";
+const SLOT_OPT: &str = "slot";
+// const RAFFLE_OPT: &str = "raffle";
 // =======================
 
 const ERROR_MSG: &str = "Application error";
@@ -36,57 +37,100 @@ fn main() {
         .version(crate_version!())
         .get_matches();
 
-    if matches.is_present(YCH_ARG)
+    if matches.is_present(YCH_FLAG)
     {
-        let ych_buyer = matches.value_of(BUYER_ARG).unwrap();
-        let ych_order = matches.value_of(ORDER_ARG).unwrap();
-        let ych_slot = matches.value_of(SLOT_ARG).unwrap();
-        let ych_cost = matches.value_of(COST_ARG).unwrap();
-        let ych_contact = matches.value_of(UNAME_ARG).unwrap();
-        let ych_payment = matches.value_of(PAYMENT_ARG).unwrap();
+        let ych_buyer = matches.value_of(BUYER_OPT).unwrap();
+        let ych_order = matches.value_of(ORDER_OPT).unwrap();
+        let ych_slot = matches.value_of(SLOT_OPT).unwrap();
+        let ych_cost = matches.value_of(COST_OPT).unwrap();
+        let ych_contact = matches.value_of(UNAME_OPT).unwrap();
+        let ych_payment = matches.value_of(PAYMENT_OPT).unwrap();
 
-        if let Err(err) = YCH::write_json(YCH {
-            id: Uuid::new_v4()
-                .to_hyphenated()
-                .to_string(),
-            date: Local::now(),
-            order: ych_order.to_owned(),
-            buyer: ych_buyer.to_owned(),
-            cost: ych_cost.to_owned(),
-            slot: ych_slot.to_owned(),
-            username: ych_contact.to_owned(),
-            payment: ych_payment.to_owned(),
-        }) {
-            println!("{}: {}", ERROR_MSG, err);
-            process::exit(exit_code);
+        if matches.is_present(DEBUG_FLAG)
+        {
+            if let Err(err) = YCH::print_ych(YCH {
+                id: Uuid::new_v4()
+                    .to_hyphenated()
+                    .to_string(),
+                date: Local::now(),
+                order: ych_order.to_owned(),
+                buyer: ych_buyer.to_owned(),
+                cost: ych_cost.to_owned(),
+                slot: ych_slot.to_owned(),
+                username: ych_contact.to_owned(),
+                payment: ych_payment.to_owned(),
+            }) {
+                println!("{}: {}", ERROR_MSG, err);
+                process::exit(exit_code);
+            }
+        }
+        else
+        {
+            if let Err(err) = YCH::write_ych(YCH {
+                id: Uuid::new_v4()
+                    .to_hyphenated()
+                    .to_string(),
+                date: Local::now(),
+                order: ych_order.to_owned(),
+                buyer: ych_buyer.to_owned(),
+                cost: ych_cost.to_owned(),
+                slot: ych_slot.to_owned(),
+                username: ych_contact.to_owned(),
+                payment: ych_payment.to_owned(),
+            }) {
+                println!("{}: {}", ERROR_MSG, err);
+                process::exit(exit_code);
+            }
         }
     }
 
-    if matches.is_present(COMM_ARG)
+    if matches.is_present(COMM_FLAG)
     {
-        let comm_buyer = matches.value_of(BUYER_ARG).unwrap();
-        let comm_order = matches.value_of(ORDER_ARG).unwrap();
-        let comm_cost = matches.value_of(COST_ARG).unwrap();
-        let comm_contact = matches.value_of(UNAME_ARG).unwrap();
-        let comm_payment = matches.value_of(PAYMENT_ARG).unwrap();
+        let comm_buyer = matches.value_of(BUYER_OPT).unwrap();
+        let comm_order = matches.value_of(ORDER_OPT).unwrap();
+        let comm_cost = matches.value_of(COST_OPT).unwrap();
+        let comm_contact = matches.value_of(UNAME_OPT).unwrap();
+        let comm_payment = matches.value_of(PAYMENT_OPT).unwrap();
 
-        if let Err(err) = Commission::write_json(Commission {
-            id: Uuid::new_v4()
-                .to_hyphenated()
-                .to_string(),
-            date: Local::now(),
-            order: comm_order.to_owned(),
-            buyer: comm_buyer.to_owned(),
-            cost: comm_cost.to_owned(),
-            username: comm_contact.to_owned(),
-            payment: comm_payment.to_owned(),
-        }) {
-            println!("{}: {}", ERROR_MSG, err);
-            process::exit(exit_code);
+        if matches.is_present(DEBUG_FLAG)
+        {
+            if let Err(err) = Commission::print_comm(Commission {
+                id: Uuid::new_v4()
+                    .to_hyphenated()
+                    .to_string(),
+                date: Local::now(),
+                order: comm_order.to_owned(),
+                buyer: comm_buyer.to_owned(),
+                cost: comm_cost.to_owned(),
+                username: comm_contact.to_owned(),
+                payment: comm_payment.to_owned(),
+            }) {
+                println!("{}: {}", ERROR_MSG, err);
+                process::exit(exit_code);
+            }
         }
+        else
+        {
+            if let Err(err) = Commission::write_comm(Commission {
+                id: Uuid::new_v4()
+                    .to_hyphenated()
+                    .to_string(),
+                date: Local::now(),
+                order: comm_order.to_owned(),
+                buyer: comm_buyer.to_owned(),
+                cost: comm_cost.to_owned(),
+                username: comm_contact.to_owned(),
+                payment: comm_payment.to_owned(),
+            }) {
+                println!("{}: {}", ERROR_MSG, err);
+                process::exit(exit_code);
+            }
+        }
+
+
     }
 
-    if matches.is_present(REQ_ARG)
+    if matches.is_present(REQ_FLAG)
     {
         unimplemented!();
         process::exit(exit_code);
