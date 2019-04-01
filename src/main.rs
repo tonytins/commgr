1841b1ca6efd6fe526e430a5557f5ca4c75f746a
Anthony Wilcox <35226681+antonwilc0x@users.noreply.github.com>
2019-04-01 08:43:25 -0400

    Moved Clap options to config file
    - Moved all Clap options to a config file in order to free space, now that I figured out that I really derped in figuring out why it didn't work before.
    - When using --help, the author, about, and version is now grabbed from the Cargo config file.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // root for full license information.
 pub mod models;
 
-use clap::{App, Arg, crate_version};
+use clap::{App, crate_version, crate_authors, crate_description, load_yaml};
 use std::process;
 use uuid::Uuid;
 use models::{YCH, Commission, Request};
@@ -15,7 +15,7 @@ const COMM_ARG: &str = "commission";
 const REQ_ARG: &str = "request";
 const YCH_ARG: &str = "ych";
 
-const NAME_ARG: &str = "name";
+const BUYER_ARG: &str = "buyer";
 const UNAME_ARG: &str = "username";
 const ORDER_ARG: &str = "order";
 const PAYMENT_ARG: &str = "payment";
@@ -29,50 +29,16 @@ const ERROR_MSG: &str = "Application error";
 fn main() {
     let exit_code = 1;
 
-    // Todo: figure out test while still keeping yaml feature enabled
-    // let yaml = load_yaml!("cli.yml");
-    // let matches = App::new(yaml).get_matches();
-
-    let matches = App::new("Art Manager")
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml)
+        .author(crate_authors!())
+        .about(crate_description!())
         .version(crate_version!())
-        .about("Request, commission, and YCH manager command line interface.")
-        .arg(Arg::with_name(YCH_ARG)
-            .long(YCH_ARG))
-        .arg(Arg::with_name(COMM_ARG)
-            .long("comm"))
-        .arg(Arg::with_name(REQ_ARG)
-            .long("req"))
-        .arg(Arg::with_name(NAME_ARG)
-            .required(true)
-            .takes_value(true)
-            .long(NAME_ARG)
-            .short("n"))
-        .arg(Arg::with_name(UNAME_ARG)
-            .required(true)
-            .takes_value(true)
-            .long("user")
-            .short("u"))
-        .arg(Arg::with_name(ORDER_ARG)
-            .takes_value(true)
-            .long("ord")
-            .short("o"))
-        .arg(Arg::with_name(COST_ARG)
-            .takes_value(true)
-            .long(COST_ARG)
-            .short("c"))
-        .arg(Arg::with_name(SLOT_ARG)
-            .takes_value(true)
-            .long(SLOT_ARG)
-            .short("s")
-            .requires(YCH_ARG))
-        .arg(Arg::with_name(PAYMENT_ARG)
-            .takes_value(true)
-            .long("pay")
-            .short("p"))
         .get_matches();
 
-    if matches.is_present(YCH_ARG) {
-        let ych_buyer = matches.value_of(NAME_ARG).unwrap();
+    if matches.is_present(YCH_ARG)
+    {
+        let ych_buyer = matches.value_of(BUYER_ARG).unwrap();
         let ych_order = matches.value_of(ORDER_ARG).unwrap();
         let ych_slot = matches.value_of(SLOT_ARG).unwrap();
         let ych_cost = matches.value_of(COST_ARG).unwrap();
@@ -98,7 +64,7 @@ fn main() {
 
     if matches.is_present(COMM_ARG)
     {
-        let comm_buyer = matches.value_of(NAME_ARG).unwrap();
+        let comm_buyer = matches.value_of(BUYER_ARG).unwrap();
         let comm_order = matches.value_of(ORDER_ARG).unwrap();
         let comm_cost = matches.value_of(COST_ARG).unwrap();
         let comm_contact = matches.value_of(UNAME_ARG).unwrap();
